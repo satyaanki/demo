e7aaf6f9fbb13b8d29196c3d5be7e3595ea46d36
satyaanki <147688288+satyaanki@users.noreply.github.com>
2024-01-12 10:32:23 +0530

    Create pallavi.py

---
+++ pallavi.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.header('puppy')https://github.com/satyaanki
+st.write('puppy')
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1WIXeC6FtKb5e2uEaj2MujcVc6YBlHXZM4A&usqp=CAU',caption='puppy')
