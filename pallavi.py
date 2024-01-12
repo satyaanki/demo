0912d529ce9ef0f6c1b1049f2c898034b14f11f7
satyaanki <147688288+satyaanki@users.noreply.github.com>
2024-01-12 10:39:35 +0530

    Update pallavi.py

--- pallavi.py
+++ pallavi.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('puppy')https://github.com/satyaanki
 st.write('puppy')
-st.image('https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.imgur.com%2FzzcVfvl.jpg&tbnid=rlcnCcZK4z6tKM&vet=12ahUKEwjum5m0hdeDAxVfTmwGHUCID6oQMygiegUIARChAQ..i&imgrefurl=https%3A%2F%2Fwww.reddit.com%2Fr%2Fpuppy%2Fcomments%2F7s5ryw%2Fomg_so_cute_and_beautiful_puppy%2F&docid=qRILWCrVTrvHJM&w=500&h=550&q=puppy%20images&ved=2ahUKEwjum5m0hdeDAxVfTmwGHUCID6oQMygiegUIARChAQ',caption='puppy')
+st.image('https://i.pinimg.com/736x/04/a9/70/04a97026abab6decaf28894ba3d9ea1b.jpg',caption='puppy')
