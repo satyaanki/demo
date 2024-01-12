6b743404f2f3202517626327c81f21e596040b43
satyaanki <147688288+satyaanki@users.noreply.github.com>
2024-01-12 10:40:22 +0530

    Update pallavi.py

--- pallavi.py
+++ pallavi.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.header('puppy')https://github.com/satyaanki
+st.header('puppy')
 st.write('puppy')
 st.image('https://i.pinimg.com/736x/04/a9/70/04a97026abab6decaf28894ba3d9ea1b.jpg',caption='puppy')
